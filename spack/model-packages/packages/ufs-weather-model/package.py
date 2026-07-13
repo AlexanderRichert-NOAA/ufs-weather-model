8b9c0f144b8949c8da362d3094d173d9a753dece
Alex Richert <alexander.richert@noaa.gov>
2026-07-13 15:43:47 -0700

    package.py: loosen ip & bacio version req's

--- spack/model-packages/packages/ufs-weather-model/package.py
+++ spack/model-packages/packages/ufs-weather-model/package.py
@@ -109,14 +109,13 @@ class UfsWeatherModel(CMakePackage):
     depends_on("w3emc")
     depends_on("nemsio", when="@:2.0.0")
     depends_on("w3nco", when="@:2.0.0")
-    depends_on("bacio@2.4.0:2.4.1", when="@develop")
     depends_on("crtm", when="@develop")
     depends_on("esmf@8.3.0:", when="@develop")
     depends_on("fms@2022.04: +deprecated_io precision=32,64 constants=GFS", when="@develop")
     depends_on("g2", when="@develop")
     depends_on("g2tmpl", when="@develop")
     depends_on("hdf5+hl+mpi", when="@develop")
-    depends_on("ip@:4", when="@develop")
+    depends_on("ip", when="@develop")
     depends_on("netcdf-c~parallel-netcdf+mpi", when="@develop")
     for app in [
         "ATMW",
